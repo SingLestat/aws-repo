5d809b6177c5e598672d2cb5fdaa2c8e1866da1f
Andrew Singley <andrewsingley12@gmail.com>
2021-07-12 11:35:15 -0400

    More fixing. added man to hangman

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -1,32 +1,33 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main extends GenWord{
+    private static int tries;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         boolean weArePlaying = true;
         while(weArePlaying){
             System.out.println("Lets Start Playing Hangman");
-            char randomWordToGuess[] = getWord().toCharArray();
+            char[] randomWordToGuess= getWord().toCharArray();
             int amountOfGuesses = randomWordToGuess.length; //total tries to guess a word.
-            char playerGuess[] = new char[amountOfGuesses]; // "_ _ _ _ _ _ _ _"
+            char[] playerGuess= new char[amountOfGuesses]; // "_ _ _ _ _ _ _ _"
 
-            for(int i=0; i<playerGuess.length; i++){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
-                playerGuess[i] =  '_';
-            }
+            // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            Arrays.fill(playerGuess,'_');
 
             boolean wordIsGuessed = false;
-            int tries = 0;
+            tries = 0;
 
             while(!wordIsGuessed && tries != amountOfGuesses){
                 System.out.println("Current Guesses: ");
                 print(playerGuess);
-                System.out.printf("You have %d ammount of tries left.\n", amountOfGuesses-tries);
+                System.out.printf("You have %d amount of tries left.\n", amountOfGuesses-tries);
                 System.out.println("Enter a single character: ");
                 char input = scanner.next().charAt(0);
                 tries++;
+                hangmanImage(randomWordToGuess.length);
 
                 if(input == '-'){
                     wordIsGuessed = true;
@@ -53,20 +54,96 @@ public class Main extends GenWord{
         System.out.println("Game Over!");
     }
 
-    public static void print(char array[]){
-        for(int i=0; i<array.length; i++){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
-            System.out.print(array[i] + " ");
+    public static void print(char[] array){
+        for(char c : array){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            System.out.print(c+" ");
         }
         System.out.println();
     }
 
     public static boolean isTheWordGuessed(char[] array){
         boolean condition = true;
-        for(int i=0; i<array.length; i++){
-            if(array[i] == '_'){
-                condition = false;
+        for(char c : array){
+            if(c=='_'){
+                condition=false;
+                break;
             }
         }
         return condition;
+    }
+    public static void hangmanImage(int wordLength) {
+        if (tries == 1) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("___|___");
+            System.out.println();
+        }
+        if (tries == 2) {
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (tries == 3) {
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   | ");
+            System.out.println("___|___");
+        }
+        if (tries == Math.abs(wordLength/2)) {
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (tries == 5) {
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (tries == 6) {
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
+        if (tries == wordLength) {
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
     }
 }
--- Numbers/src/Numbers.java
+++ Numbers/src/Numbers.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,40 +6,54 @@ public class Numbers {
     public static Scanner scan = new Scanner(System.in);
     public static Random rand = new Random();
 
+    public static int tries = 0, ranNum, num;
+    public static String name;
+
     public static void main(String[] args) {
-        int tries = 0, ranNum, num;
-        String name;
-        String reset;
 
         ranNum = rand.nextInt(20) + 1;
 
         System.out.println("What is your name?");
         name = scan.nextLine();
 
-        while (tries != 6) {
-            System.out.println("Well," + name + ", I am thinking of a number between 1 and 20.");
-            num = scan.nextInt();
-
-
-            if (num == ranNum) {
-                System.out.println("You got it!\n");
-                System.out.println("Would you like to play again? (y or n)\n");
-                reset = scan.nextLine();
-
-                if (reset == "y" || reset == "n")
-                {
-                    tries = 0;
-                }
-                else {
-                    break;
-                }
-
-            } else if (num > 20) {
-                System.out.println("Your number needs to be between 1 and 20.");
-            } else {
-                System.out.println("Try again!");
+        while (true) {
+            if (tries != 6){
+                gameLoop();
                 tries++;
             }
+            else {
+                setReset();
+            }
+        }
+    }
+
+    public static void gameLoop(){
+        System.out.println("Well," + name + ", I am thinking of a number between 1 and 20.");
+
+        num = scan.nextInt();
+        
+        if (num == ranNum) {
+            System.out.println("You got it!\n");
+            setReset();
+        } else if (num > 20) {
+            System.out.println("Your number needs to be between 1 and 20.");
+        } else {
+            System.out.println("Try again!");
+        }
+    }
+
+    public static void setReset(){
+        System.out.println("Want to play again?(Y or N)");
+        String Choice = scan.nextLine();
+        if (Choice.equals("Y")){
+            tries = 0;
+        }
+        if (Choice.equals("N")){
+            System.exit(0);
+        }
+        else {
+            System.out.println("Invalid Choice.");
+            setReset();
         }
     }
 }
